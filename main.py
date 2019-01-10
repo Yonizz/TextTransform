562e626f4c1c455ab05730f50b7f2bc8cba26cfd
ShaBang <baptiste.perrier@protonmail.com>
2019-01-10 19:23:28 +0100

    Added support for multi-combinaison, and the possibility to import the function from an external module

--- main.py
+++ main.py
@@ -26,37 +26,44 @@ def GetClipboardData():
     win32clipboard.CloseClipboard()
     return data
 
-def TransformText(data):
-    return data.upper()
+def TransformText(data, transform):
+    global COMBINAISONS
 
-def UpdateClipboard():
+    return COMBINAISONS[transform][1](data)
+
+def UpdateClipboard(transform):
     win32clipboard.OpenClipboard()
     data = win32clipboard.GetClipboardData()
-    data = TransformText(data)
+    data = TransformText(data, transform)
     win32clipboard.SetClipboardText(data, win32clipboard.CF_UNICODETEXT)
     win32clipboard.CloseClipboard()
 
 def on_press(key):
-    global activation
+    global activation, transform
+    if key in [keys for combinaison in COMBINAISONS for keys in combinaison[0]]:
 
-    if key in COMBINAISON:
-        currentPressed.add(key)     #if the key is in the combinaison, we update the setKey all(k in currentPressed for k in COMBINAISON):
-        if all(k in currentPressed for k in COMBINAISON):
-            activation = True       #if every key is pressed, we launch the action on release
+        currentPressed.add(key)     #if the key is in the combinaison, we update the set Keys
+
+        for i in range(0,len(COMBINAISONS)):
+            if all(k in currentPressed for k in COMBINAISONS[i][0]):
+                activation = True       #if every key is pressed, we launch the action on release
+                print(COMBINAISONS[0][0], i)
+                transform = i
+                print(transform)
 
     if key == Key.esc:
         print("You ve stopped the listener")
         listener.stop()
 
 def on_release(key):
-    global activation
+    global activation, transform
 
     try:
         currentPressed.remove(key)  #if the current released key is in the combinaison, it is removed of the set, because it's released
         if len(currentPressed) == 0 and activation:     #if every key of the combinaison have been pressed (activation) and released (len() == 0)
             kCopyText(keyboard)
             time.sleep(0.2)
-            UpdateClipboard()
+            UpdateClipboard(transform)
             time.sleep(0.2)
             kPasteText(keyboard)
             time.sleep(0.2)
